cae41c3554834bf15494a31c4d611e58d8fccabd
Connor <cnnrznn@udel.edu>
2018-12-20 08:32:13 -0500

    request the audio analysis for a song

--- spotify-reactor/spotify-react/src/spotify_react/core.clj
+++ spotify-reactor/spotify-react/src/spotify_react/core.clj
@@ -1,10 +1,12 @@
 (ns spotify-react.core
   (:require [clj-spotify [util :as spu]
-                         [core :as spc]]
+                         [core :as spc]
+                         [analysis :as spa]]
             [clojure.pprint :as pp]))
 
 (def client-id "29b14ddace06413b96fb9c904c89b706")
 (def client-secret "f5a1c1087457445fb48181517ece5605")
+(def token (spu/get-access-token client-id client-secret))
 
 (defn track-tuple
   [{:keys [id name artists]}]
@@ -14,8 +16,8 @@
                first
                :name)})
 
-(defn get-song
-  [song token]
+(defn search-track
+  [song]
   (let [query {:q song
                :type "track"
                :limit 1}]
@@ -25,8 +27,21 @@
         first
         track-tuple)))
 
+(defn req-track
+  [tid]
+  (let [query {:id tid}]
+    (spc/get-a-track query token)))
+
+(defn req-analysis
+  [tid]
+  (let [query {:id tid}]
+    (pp/pprint query)
+    (spa/get-audio-analysis query token)))
+
 (defn -main
   [& args]
-  (->> (spu/get-access-token client-id client-secret)
-       (get-song "high hopes")
+  (->> "high hopes"
+       (search-track)
+       :id
+       req-analysis
        pp/pprint))
